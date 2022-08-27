22e6f2e00ba40347b08df2a91fd101a33aadbaf9
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-08-27 11:47:11 +0500

    Add button to add fields

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import InputField from "./components/InputField";
 import "./styles/app.css";
 
 function App() {
-  const [fields, setfields] = useState([
+  const [fields, setFields] = useState([
     { id: 0, text: "" },
     { id: 1, text: "" },
   ]);
+
+  const addField = () => {
+    const updatedFields = [...fields, { id: fields.length, text: "" }];
+    setFields(updatedFields);
+  };
 
   return (
     <div className="app">
       {fields.map((field) => {
         return <InputField />;
       })}
+      <button className="add-btn" onClick={addField}>
+        Add Text Field
+      </button>
     </div>
   );
 }
