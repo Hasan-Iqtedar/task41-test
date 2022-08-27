8bfadae41a4f9e8510dc244e431fb6cf9a854b6c
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-08-27 11:40:30 +0500

    Display text fields

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import InputField from "./components/InputField";
 import "./styles/app.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [fields, setfields] = useState([
+    { id: 0, text: "" },
+    { id: 1, text: "" },
+  ]);
+
+  return (
+    <div className="app">
+      {fields.map((field) => {
+        return <InputField />;
+      })}
+    </div>
+  );
 }
 
 export default App;
